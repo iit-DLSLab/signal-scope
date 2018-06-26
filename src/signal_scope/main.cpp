2aa31c3f8a9a44ae1a50e37602d9c799eea3e197
Marco Camurri <marco.camurri@iit.it>
2018-06-26 14:00:27 +0200

    handling the additional arguments caused by ros launchfile

--- src/signal_scope/main.cpp
+++ src/signal_scope/main.cpp
@@ -12,7 +12,23 @@
 int main(int argc, char **argv)
 {
 
-  QApplication app(argc, argv);
+    // Input sanity check because roslaunch adds extra args we don't want
+    int myargc = argc;
+    std::string myarg;
+    for(int i=0; i < argc; i++){
+        // if the argument starts with __name:= or __log:= we decrease the count
+        // NOTE: this only works if the unwanted arguments are the last ones
+        myarg = std::string(argv[i]);
+        if(myarg.substr(0,strlen("__name:=")).compare("__name:=") == 0){
+            myargc--;
+        }
+        if(myarg.substr(0,strlen("__log:=")).compare("__log:=") == 0){
+            myargc--;
+        }
+    }
+
+
+  QApplication app(myargc, argv);
 
   MainWindow window;
   window.show();
